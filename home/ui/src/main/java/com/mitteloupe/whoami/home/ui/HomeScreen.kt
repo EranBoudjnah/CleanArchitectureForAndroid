b0d1a169b17ded6d261a3d94b0157795b5b1e9ef
Eran Boudjnah <eranbou@gmail.com>
2023-08-23 12:01:05 +0100

    Added animations. Tidied up code. Updated readme.

--- home/ui/src/main/java/com/mitteloupe/whoami/home/ui/HomeScreen.kt
+++ home/ui/src/main/java/com/mitteloupe/whoami/home/ui/HomeScreen.kt
@@ -1,8 +1,18 @@
 package com.mitteloupe.whoami.home.ui
 
-import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +26,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,8 +36,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mitteloupe.whoami.analytics.Analytics
@@ -74,19 +87,7 @@ fun Home(
     when (val notificationValue = notification.value) {
         null -> Unit
 
-        is ConnectionSaved -> {
-            val context = LocalContext.current
-            LaunchedEffect(notification) {
-                Toast.makeText(
-                    context,
-                    context.getString(
-                        R.string.home_details_saved_notification,
-                        notificationValue.ipAddress
-                    ),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
+        is ConnectionSaved -> NotificationToast(notification, notificationValue)
     }
 
     navigation.value?.apply {
@@ -116,67 +117,48 @@ fun Home(
                 coroutineContextProvider.main
             )
 
-            when (val viewStateValue = viewState.value) {
-                is HomeViewState.Loading -> {
-                    Box(
-                        modifier = modifier
-                            .fillMaxSize()
-                            .weight(1f),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        LoadingAnimation()
-                    }
-                }
+            val lastConnectedState = remember { mutableStateOf<HomeViewState.Connected?>(null) }
+            val viewStateValue = viewState.value
 
-                is HomeViewState.Connected -> {
-                    ConnectedContent(
-                        connectionDetails = connectionDetailsToUiMapper.toUi(viewStateValue),
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                }
-
-                is HomeViewState.Disconnected -> {
-                    DisconnectedContent(modifier = Modifier.fillMaxWidth())
-                }
-
-                is HomeViewState.Error -> {
-                    ErrorContent(modifier = Modifier.fillMaxWidth())
-                }
+            if (viewStateValue is HomeViewState.Connected) {
+                lastConnectedState.value = viewStateValue
             }
+
+            LoadingAnimationContainer(viewStateValue)
+
+            ConnectedContentContainer(
+                viewStateValue,
+                lastConnectedState,
+                connectionDetailsToUiMapper
+            )
+
+            DisconnectedContentContainer(viewStateValue)
+
+            ErrorContentContainer(viewStateValue)
 
             Row(
                 modifier = Modifier
                     .padding(start = 16.dp, top = 24.dp, end = 16.dp, bottom = 24.dp)
                     .fillMaxWidth()
             ) {
-                when (val viewStateValue = viewState.value) {
-                    is HomeViewState.Connected -> {
-                        NavigationButton(
-                            iconResourceId = R.drawable.icon_save,
-                            label = stringResource(R.string.home_save_details_button_label),
-                            onClick = {
-                                analytics.logEvent(
-                                    Click("Save Details", mapOf("State" to "Connected"))
-                                )
-                                homeViewModel.onSaveDetails(viewStateValue)
-                            },
-                            modifier = Modifier.weight(1f)
-                        )
-                    }
-
-                    else -> {
-                        NavigationButton(
-                            iconResourceId = R.drawable.icon_save,
-                            label = stringResource(R.string.home_save_details_button_label),
-                            onClick = {
-                                analytics.logEvent(
-                                    Click("Save Details", mapOf("State" to "Not connected"))
-                                )
-                            },
-                            modifier = Modifier.weight(1f)
-                        )
-                    }
-                }
+                NavigationButton(
+                    iconResourceId = R.drawable.icon_save,
+                    label = stringResource(R.string.home_save_details_button_label),
+                    onClick = {
+                        val viewStateValue = viewState.value
+                        if (viewStateValue is HomeViewState.Connected) {
+                            analytics.logEvent(
+                                Click("Save Details", mapOf("State" to "Connected"))
+                            )
+                            homeViewModel.onSaveDetails(viewStateValue)
+                        } else {
+                            analytics.logEvent(
+                                Click("Save Details", mapOf("State" to "Not connected"))
+                            )
+                        }
+                    },
+                    modifier = Modifier.weight(1f)
+                )
                 Spacer(modifier = Modifier.width(16.dp))
                 NavigationButton(
                     iconResourceId = R.drawable.icon_history,
@@ -189,5 +171,84 @@ fun Home(
                 )
             }
         }
+    }
+}
+
+@Composable
+private fun ColumnScope.LoadingAnimationContainer(
+    viewStateValue: HomeViewState,
+    modifier: Modifier = Modifier
+) {
+    AnimatedVisibility(
+        visible = viewStateValue is HomeViewState.Loading,
+        enter = expandVertically() + fadeIn(),
+        exit = shrinkVertically() + fadeOut()
+    ) {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            LoadingAnimation()
+        }
+    }
+}
+
+@Stable
+private fun <T> enterSpring(visibilityThreshold: T) = spring(
+    dampingRatio = Spring.DampingRatioLowBouncy,
+    stiffness = Spring.StiffnessMediumLow,
+    visibilityThreshold = visibilityThreshold
+)
+
+@Composable
+private fun ColumnScope.ConnectedContentContainer(
+    viewStateValue: HomeViewState,
+    lastConnectedState: MutableState<HomeViewState.Connected?>,
+    connectionDetailsToUiMapper: ConnectionDetailsToUiMapper
+) {
+    AnimatedVisibility(
+        visible = viewStateValue is HomeViewState.Connected,
+        enter = expandVertically(
+            animationSpec = enterSpring(IntSize.VisibilityThreshold)
+        ) + slideInVertically(
+            animationSpec = enterSpring(IntOffset.VisibilityThreshold)
+        ) + fadeIn(),
+        exit = shrinkVertically() + slideOutVertically() + fadeOut()
+    ) {
+        val connectedState = requireNotNull(lastConnectedState.value)
+        ConnectedContent(
+            connectionDetails = connectionDetailsToUiMapper.toUi(connectedState),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+private fun ColumnScope.DisconnectedContentContainer(viewStateValue: HomeViewState) {
+    AnimatedVisibility(
+        visible = viewStateValue is HomeViewState.Disconnected,
+        enter = expandVertically(
+            animationSpec = enterSpring(IntSize.VisibilityThreshold)
+        ) + slideInVertically(
+            animationSpec = enterSpring(IntOffset.VisibilityThreshold)
+        )
+    ) {
+        DisconnectedContent(modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+private fun ColumnScope.ErrorContentContainer(viewStateValue: HomeViewState) {
+    AnimatedVisibility(
+        visible = viewStateValue is HomeViewState.Error,
+        enter = expandVertically(
+            animationSpec = enterSpring(IntSize.VisibilityThreshold)
+        ) + slideInVertically(
+            animationSpec = enterSpring(IntOffset.VisibilityThreshold)
+        )
+    ) {
+        ErrorContent(modifier = Modifier.fillMaxWidth())
     }
 }
