749f5b8a54930901265c02c55c13404998a4436b
Eran Boudjnah <eranbou@gmail.com>
2023-09-08 18:48:06 +0100

    Migrated to version catalog.

--- home/ui/build.gradle.kts
+++ home/ui/build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
 
     implementation(projects.analytics)
 
-    implementation(platform("androidx.compose:compose-bom:2023.09.00"))
+    implementation(platform(libs.compose.bom))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
