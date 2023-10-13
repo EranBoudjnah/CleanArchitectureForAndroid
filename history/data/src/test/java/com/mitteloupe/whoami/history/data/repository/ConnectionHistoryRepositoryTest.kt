200bfc08b61f902701c3ce61487a753b337416e3
Eran Boudjnah <eranbou@gmail.com>
2023-10-13 09:59:57 +0100

    Implemented the record deletion data layer.

--- history/data/src/test/java/com/mitteloupe/whoami/history/data/repository/ConnectionHistoryRepositoryTest.kt
+++ history/data/src/test/java/com/mitteloupe/whoami/history/data/repository/ConnectionHistoryRepositoryTest.kt
@@ -1,8 +1,11 @@
 package com.mitteloupe.whoami.history.data.repository
 
 import com.mitteloupe.whoami.datasource.history.datasource.IpAddressHistoryDataSource
+import com.mitteloupe.whoami.datasource.history.model.HistoryRecordDeletionIdentifierDataModel
 import com.mitteloupe.whoami.datasource.history.model.SavedIpAddressHistoryRecordDataModel
+import com.mitteloupe.whoami.history.data.mapper.HistoryRecordDeletionToDataMapper
 import com.mitteloupe.whoami.history.data.mapper.SavedIpAddressRecordToDomainMapper
+import com.mitteloupe.whoami.history.domain.model.HistoryRecordDeletionDomainModel
 import com.mitteloupe.whoami.history.domain.model.SavedIpAddressRecordDomainModel
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.collection.IsCollectionWithSize.hasSize
@@ -13,6 +16,7 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.given
+import org.mockito.kotlin.verify
 
 @RunWith(MockitoJUnitRunner::class)
 class ConnectionHistoryRepositoryTest {
@@ -24,10 +28,16 @@ class ConnectionHistoryRepositoryTest {
     @Mock
     private lateinit var connectionDetailsToDataMapper: SavedIpAddressRecordToDomainMapper
 
+    @Mock
+    private lateinit var recordDeletionToDataMapper: HistoryRecordDeletionToDataMapper
+
     @Before
     fun setUp() {
-        classUnderTest =
-            ConnectionHistoryRepository(ipAddressHistoryDataSource, connectionDetailsToDataMapper)
+        classUnderTest = ConnectionHistoryRepository(
+            ipAddressHistoryDataSource,
+            connectionDetailsToDataMapper,
+            recordDeletionToDataMapper
+        )
     }
 
     @Test
@@ -50,6 +60,21 @@ class ConnectionHistoryRepositoryTest {
         // Then
         assertThat(actualHistory, hasSize(2))
         assertThat(actualHistory, containsInAnyOrder(domainHistoryRecord1, domainHistoryRecord2))
+    }
+
+    @Test
+    fun `Given deletion request when delete then deletes requested record`() {
+        // Given
+        val givenIpAddress = "1.1.1.1"
+        val givenRequest = HistoryRecordDeletionDomainModel(givenIpAddress)
+        val dataRequest = HistoryRecordDeletionIdentifierDataModel(givenIpAddress)
+        given(recordDeletionToDataMapper.toData(givenRequest)).willReturn(dataRequest)
+
+        // When
+        classUnderTest.delete(givenRequest)
+
+        // Then
+        verify(ipAddressHistoryDataSource).delete(dataRequest)
     }
 
     private fun dataHistoryRecord(ipAddress: String) = SavedIpAddressHistoryRecordDataModel(
