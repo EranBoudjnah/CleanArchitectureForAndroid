ae0b74690c48716f98b5f4efdbea57be53240b4f
Eran Boudjnah <eranbou@gmail.com>
2023-11-08 13:31:34 +0000

    Upgrade Kotlin and Compose (#85)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
     packaging {
         resources {
