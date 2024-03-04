b001a2acf81fc780dbf6d69f3a599fec7873bfcd
Eran Boudjnah <eranbou@gmail.com>
2024-03-04 16:18:48 +0000

    Used junit.framework.AssertionFailedError.

--- architecture/instrumentation-test/src/main/java/com/mitteloupe/whoami/test/assertion/ItemAtPositionMatcher.kt
+++ architecture/instrumentation-test/src/main/java/com/mitteloupe/whoami/test/assertion/ItemAtPositionMatcher.kt
@@ -4,6 +4,7 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import junit.framework.AssertionFailedError
 import org.hamcrest.Matcher
 
 fun matchesItemAtPosition(matcher: Matcher<View?>?, position: Int) =
@@ -13,6 +14,6 @@ fun matchesItemAtPosition(matcher: Matcher<View?>?, position: Int) =
         }
         val recyclerView = view as RecyclerView
         val viewHolder = recyclerView.findViewHolderForAdapterPosition(position)
-            ?: throw AssertionError("No view holder at position: $position")
+            ?: throw AssertionFailedError("No view holder at position: $position")
         assertThat(viewHolder.itemView, matcher)
     }
