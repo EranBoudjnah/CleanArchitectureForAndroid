8853651ebe7cc4cccde5b9d8c7f9c86693d9a36f
Eran Boudjnah <eranbou@gmail.com>
2024-09-08 13:28:02 +0100

    Used error instead of IllegalStateException.

--- architecture/instrumentation-test/src/main/java/com/mitteloupe/whoami/test/test/DoesNot.kt
+++ architecture/instrumentation-test/src/main/java/com/mitteloupe/whoami/test/test/DoesNot.kt
@@ -5,7 +5,7 @@ import junit.framework.AssertionFailedError
 fun doesNot(description: String, block: () -> Unit) {
     try {
         block()
-        throw IllegalStateException("Unexpected: $description")
+        error("Unexpected: $description")
     } catch (ignore: AssertionFailedError) {
     }
 }
