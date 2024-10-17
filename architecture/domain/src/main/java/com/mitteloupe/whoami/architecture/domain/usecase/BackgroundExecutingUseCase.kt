38f70577bd56f697de1ec0dfe9af05df9a606f13
Eran Boudjnah <eranbou@gmail.com>
2024-10-17 11:11:51 +0100

    Extracted coroutines from UseCase.

--- architecture/domain/src/main/java/com/mitteloupe/whoami/architecture/domain/usecase/BackgroundExecutingUseCase.kt
+++ architecture/domain/src/main/java/com/mitteloupe/whoami/architecture/domain/usecase/BackgroundExecutingUseCase.kt
@@ -1,16 +1,26 @@
 package com.mitteloupe.whoami.architecture.domain.usecase
 
 import com.mitteloupe.whoami.coroutine.CoroutineContextProvider
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 abstract class BackgroundExecutingUseCase<REQUEST, RESULT>(
-    private val coroutineContextProvider: CoroutineContextProvider
+    private val coroutineContextProvider: CoroutineContextProvider,
+    private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Main)
 ) : UseCase<REQUEST, RESULT> {
-    final override suspend fun execute(input: REQUEST, onResult: (RESULT) -> Unit) {
-        val result = withContext(coroutineContextProvider.io) {
-            executeInBackground(input)
+    final override fun execute(input: REQUEST, onResult: (RESULT) -> Unit) {
+        try {
+            coroutineScope.launch {
+                val result = withContext(coroutineContextProvider.io) {
+                    executeInBackground(input)
+                }
+                onResult(result)
+            }
+        } catch (_: CancellationException) {
         }
-        onResult(result)
     }
 
     abstract fun executeInBackground(request: REQUEST): RESULT
--- architecture/domain/src/main/java/com/mitteloupe/whoami/architecture/domain/usecase/ContinuousExecutingUseCase.kt
+++ architecture/domain/src/main/java/com/mitteloupe/whoami/architecture/domain/usecase/ContinuousExecutingUseCase.kt
@@ -1,19 +1,29 @@
 package com.mitteloupe.whoami.architecture.domain.usecase
 
 import com.mitteloupe.whoami.coroutine.CoroutineContextProvider
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 abstract class ContinuousExecutingUseCase<REQUEST, RESULT>(
-    private val coroutineContextProvider: CoroutineContextProvider
+    private val coroutineContextProvider: CoroutineContextProvider,
+    private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Main)
 ) : UseCase<REQUEST, RESULT> {
-    final override suspend fun execute(input: REQUEST, onResult: (RESULT) -> Unit) {
-        withContext(coroutineContextProvider.io) {
-            executeInBackground(input).collect { result ->
-                withContext(coroutineContextProvider.main) {
-                    onResult(result)
+    final override fun execute(input: REQUEST, onResult: (RESULT) -> Unit) {
+        try {
+            coroutineScope.launch {
+                withContext(coroutineContextProvider.io) {
+                    executeInBackground(input).collect { result ->
+                        withContext(coroutineContextProvider.main) {
+                            onResult(result)
+                        }
+                    }
                 }
             }
+        } catch (_: CancellationException) {
         }
     }
 
