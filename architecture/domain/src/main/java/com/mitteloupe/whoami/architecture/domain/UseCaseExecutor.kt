5d35f1817ddd271f492d181cc2bf46062aa47683
Eran Boudjnah <eranbou@gmail.com>
2024-10-23 14:40:16 +0100

    Extracted coroutines from UseCase. (#281)

--- architecture/domain/src/main/java/com/mitteloupe/whoami/architecture/domain/UseCaseExecutor.kt
+++ architecture/domain/src/main/java/com/mitteloupe/whoami/architecture/domain/UseCaseExecutor.kt
@@ -3,14 +3,8 @@ package com.mitteloupe.whoami.architecture.domain
 import com.mitteloupe.whoami.architecture.domain.exception.DomainException
 import com.mitteloupe.whoami.architecture.domain.exception.UnknownDomainException
 import com.mitteloupe.whoami.architecture.domain.usecase.UseCase
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
-class UseCaseExecutor(
-    private val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Main)
-) {
+class UseCaseExecutor {
     fun <OUTPUT> execute(
         useCase: UseCase<Unit, OUTPUT>,
         onResult: (OUTPUT) -> Unit = {},
@@ -23,16 +17,12 @@ class UseCaseExecutor(
         onResult: (OUTPUT) -> Unit = {},
         onException: (DomainException) -> Unit = {}
     ) {
-        coroutineScope.launch {
-            try {
-                useCase.execute(value, onResult)
-            } catch (_: CancellationException) {
-            } catch (@Suppress("TooGenericExceptionCaught") throwable: Throwable) {
-                onException(
-                    (throwable as? DomainException)
-                        ?: UnknownDomainException(throwable)
-                )
-            }
+        try {
+            useCase.execute(value, onResult)
+        } catch (@Suppress("TooGenericExceptionCaught") throwable: Throwable) {
+            val domainException =
+                ((throwable as? DomainException) ?: UnknownDomainException(throwable))
+            onException(domainException)
         }
     }
 }
