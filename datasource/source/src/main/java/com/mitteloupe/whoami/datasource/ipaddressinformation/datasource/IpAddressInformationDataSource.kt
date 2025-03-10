c72a513c5a2f29948e69447a3474d883c59e57af
Eran Boudjnah <eranbou@gmail.com>
2025-03-10 16:39:02 +0000

    Added @Throws annotation to data source functions. (#364)

--- datasource/source/src/main/java/com/mitteloupe/whoami/datasource/ipaddressinformation/datasource/IpAddressInformationDataSource.kt
+++ datasource/source/src/main/java/com/mitteloupe/whoami/datasource/ipaddressinformation/datasource/IpAddressInformationDataSource.kt
@@ -1,7 +1,9 @@
 package com.mitteloupe.whoami.datasource.ipaddressinformation.datasource
 
+import com.mitteloupe.whoami.datasource.ipaddressinformation.exception.NoIpAddressInformationDataException
 import com.mitteloupe.whoami.datasource.ipaddressinformation.model.IpAddressInformationDataModel
 
 interface IpAddressInformationDataSource {
+    @Throws(NoIpAddressInformationDataException::class)
     fun ipAddressInformation(ipAddress: String): IpAddressInformationDataModel
 }
