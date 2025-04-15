c0508a964df957edfb3326e45e54ce1cb3b135fb
Eran Boudjnah <eranbou@gmail.com>
2025-04-15 13:21:12 +0100

    Simplified AssetReader further.

--- architecture/instrumentation-test/src/main/java/com/mitteloupe/whoami/test/asset/AssetReader.kt
+++ architecture/instrumentation-test/src/main/java/com/mitteloupe/whoami/test/asset/AssetReader.kt
@@ -2,12 +2,11 @@ package com.mitteloupe.whoami.test.asset
 
 import androidx.test.platform.app.InstrumentationRegistry
 import java.io.InputStream
-import java.io.InputStreamReader
 
 object AssetReader {
     fun getAssetAsString(name: String): String =
         javaClass.classLoader!!.getResourceAsStream("assets/$name").use { stream ->
-            InputStreamReader(stream).use(InputStreamReader::readText)
+            stream.bufferedReader().readText()
         }
 }
 
