b57c9df2a634f60d7183ab9dd01f166d7122e7b3
Eran Boudjnah <eranbou@gmail.com>
2025-04-15 13:03:44 +0100

    Simplified AssetReader further.

--- architecture/instrumentation-test/src/main/java/com/mitteloupe/whoami/test/asset/AssetReader.kt
+++ architecture/instrumentation-test/src/main/java/com/mitteloupe/whoami/test/asset/AssetReader.kt
@@ -7,18 +7,12 @@ import java.io.InputStreamReader
 object AssetReader {
     fun getAssetAsString(name: String): String =
         javaClass.classLoader!!.getResourceAsStream("assets/$name").use { stream ->
-            InputStreamReader(stream).use { reader ->
-                reader.readText()
-            }
+            InputStreamReader(stream).use(InputStreamReader::readText)
         }
 }
 
 fun <OUTPUT> processAssetStream(
     filename: String,
     performOnStream: (inputString: InputStream) -> OUTPUT
-): OUTPUT {
-    val stream = InstrumentationRegistry.getInstrumentation().context.assets.open(filename)
-    val result = performOnStream(stream)
-    stream.close()
-    return result
-}
+): OUTPUT = InstrumentationRegistry.getInstrumentation().context.assets.open(filename)
+    .use { stream -> performOnStream(stream) }
