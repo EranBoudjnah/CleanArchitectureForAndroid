171f55277ec8935daf82b9461a858c98acfb89bd
Eran Boudjnah <eranbou@gmail.com>
2025-04-21 09:26:12 +0100

    Tidied up WebServerRule. (#414)
    * Tidied up WebServerRule.
    * Broke down WebServerStatement#evaluate down into private functions.

--- architecture/instrumentation-test/src/main/java/com/mitteloupe/whoami/test/rule/WebServerRule.kt
+++ architecture/instrumentation-test/src/main/java/com/mitteloupe/whoami/test/rule/WebServerRule.kt
@@ -1,6 +1,7 @@
 package com.mitteloupe.whoami.test.rule
 
 import com.mitteloupe.whoami.test.annotation.ServerRequestResponse
+import com.mitteloupe.whoami.test.server.MockRequest
 import com.mitteloupe.whoami.test.server.ResponseBinder
 import com.mitteloupe.whoami.test.server.ResponseStore
 import org.junit.rules.TestRule
@@ -12,54 +13,54 @@ class WebServerRule(
     private val lazyResponseStore: Lazy<ResponseStore>
 ) : TestRule {
     override fun apply(base: Statement, description: Description): Statement =
-        WebServerInitializationStatement(
-            lazyMockDispatcher,
-            lazyResponseStore,
-            base,
-            description
-        )
+        WebServerStatement(lazyMockDispatcher, lazyResponseStore, base, description)
+}
 
-    private class WebServerInitializationStatement(
-        private val lazyMockDispatcher: Lazy<ResponseBinder>,
-        private val lazyResponseStore: Lazy<ResponseStore>,
-        private val base: Statement,
-        private val description: Description
-    ) : Statement() {
-        val responseStore by lazy { lazyResponseStore.value }
-        val mockDispatcher by lazy { lazyMockDispatcher.value }
+private class WebServerStatement(
+    private val lazyMockDispatcher: Lazy<ResponseBinder>,
+    private val lazyResponseStore: Lazy<ResponseStore>,
+    private val base: Statement,
+    private val description: Description
+) : Statement() {
+    val responseStore by lazy { lazyResponseStore.value }
+    val mockDispatcher by lazy { lazyMockDispatcher.value }
 
-        override fun evaluate() {
-            val requestResponses = description.requestResponseIds()
-                .map { requestResponseId ->
-                    responseStore.responseFactories[requestResponseId]
-                        ?: throw IllegalArgumentException(
-                            "Request/Response ID $requestResponseId not found."
-                        )
-                }
-
-            mockDispatcher.testName = description.displayName
-            requestResponses.forEach { requestResponse ->
-                mockDispatcher.bindResponse(requestResponse)
-            }
-            val stubbedResponseKeys = requestResponses
-                .map { requestResponse -> requestResponse.request }
-                .toSet()
-
-            base.evaluate()
-
-            val usedResponseKeys = mockDispatcher.usedEndpoints.toSet()
-
-            val unusedResponseKeys = stubbedResponseKeys - usedResponseKeys
-            check(unusedResponseKeys.isEmpty()) {
-                "${unusedResponseKeys.size} unused stubbed URLs:\n[" +
-                    unusedResponseKeys.joinToString("]\n[") + "]"
-            }
-
-            mockDispatcher.reset()
-        }
-
-        private fun Description.requestResponseIds() =
-            annotations.filterIsInstance<ServerRequestResponse>()
-                .flatMap { serverResponse -> serverResponse.requestResponseIds.toList() }
+    override fun evaluate() {
+        mockDispatcher.testName = description.displayName
+        val stubbedResponseKeys = bindRequestResponseFactories()
+        base.evaluate()
+        assertAllStubsUsed(stubbedResponseKeys)
+        mockDispatcher.reset()
     }
+
+    private fun bindRequestResponseFactories(): Set<MockRequest> {
+        val requestResponses = description.requestResponseIds()
+            .map { requestResponseId ->
+                requireNotNull(
+                    responseStore.responseFactories[requestResponseId]
+                ) { "Request/Response ID $requestResponseId not found." }
+            }
+
+        requestResponses.forEach { requestResponse ->
+            mockDispatcher.bindResponse(requestResponse)
+        }
+        val stubbedResponseKeys = requestResponses
+            .map { requestResponse -> requestResponse.request }
+            .toSet()
+        return stubbedResponseKeys
+    }
+
+    private fun assertAllStubsUsed(stubbedResponseKeys: Set<MockRequest>) {
+        val usedResponseKeys = mockDispatcher.usedEndpoints.toSet()
+
+        val unusedResponseKeys = stubbedResponseKeys - usedResponseKeys
+        check(unusedResponseKeys.isEmpty()) {
+            "${unusedResponseKeys.size} unused stubbed URLs:\n[" +
+                unusedResponseKeys.joinToString("]\n[") + "]"
+        }
+    }
+
+    private fun Description.requestResponseIds() =
+        annotations.filterIsInstance<ServerRequestResponse>()
+            .flatMap { serverResponse -> serverResponse.requestResponseIds.toList() }
 }
