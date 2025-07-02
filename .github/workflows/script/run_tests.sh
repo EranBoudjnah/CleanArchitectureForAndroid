032ea42621549bf3e32a55ab8eed58340263ac7c
Eran Boudjnah <eranbou@gmail.com>
2025-07-02 13:54:13 +0100

    Changed gradlew command to assemble the Espresso build.

--- .github/workflows/script/run_tests.sh
+++ .github/workflows/script/run_tests.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 set +e
-./gradlew assembleDebug assembleDebugAndroidTest --no-daemon
-pwd
-adb install ../../app/build/outputs/apk/espresso/app-espresso.apk
-adb install ../../app/build/outputs/apk/androidTest/espresso/app-espresso-androidTest.apk
+./gradlew assembleEspresso assembleEspressoAndroidTest --no-daemon
+adb install app/build/outputs/apk/espresso/app-espresso.apk
+adb install app/build/outputs/apk/androidTest/espresso/app-espresso-androidTest.apk
 adb shell am instrument -w -m -e debug false \\n -e class 'com.mitteloupe.whoami.suite.SmokeTests' \\ncom.mitteloupe.whoami.test/com.mitteloupe.whoami.di.HiltTestRunner
 GRADLE_EXIT_CODE=$?
 set -e
