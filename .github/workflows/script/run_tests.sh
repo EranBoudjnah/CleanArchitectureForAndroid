fa6c9112211f6fc5fb1cd7de25511118a4a99061
Eran Boudjnah <eranbou@gmail.com>
2025-07-02 18:32:09 +0100

    Reopening after accidental merge. (#456)

--- .github/workflows/script/run_tests.sh
+++ .github/workflows/script/run_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 set +e
-./gradlew assembleDebug assembleDebugAndroidTest --no-daemon
+./gradlew assembleEspresso assembleEspressoAndroidTest --no-daemon
 adb install app/build/outputs/apk/espresso/app-espresso.apk
 adb install app/build/outputs/apk/androidTest/espresso/app-espresso-androidTest.apk
-adb shell am instrument -w -m -e debug false \\n -e class 'com.mitteloupe.whoami.suite.SmokeTests' \\ncom.mitteloupe.whoami.test/com.mitteloupe.whoami.di.HiltTestRunner
+adb shell am instrument -w -m -e debug false -e class 'com.mitteloupe.whoami.suite.SmokeTests' com.mitteloupe.whoami.test/com.mitteloupe.whoami.di.HiltTestRunner
 GRADLE_EXIT_CODE=$?
 set -e
 
