e9e203170167b5b35d67eeb8c112c6d347476c3f
Eran Boudjnah <eranbou@gmail.com>
2025-08-22 16:52:24 +0100

    Simplified build.gradle.kts files.

--- architecture/instrumentation-test/build.gradle.kts
+++ architecture/instrumentation-test/build.gradle.kts
@@ -27,14 +27,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
     }
 }
 
